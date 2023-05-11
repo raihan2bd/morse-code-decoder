d8001b101ae2ce323d4a09f00518d29c8f5d8a33
Abu Raihan <raihan2bd.official@gmail.com>
2023-05-11 10:58:06 +0600

    Add Morse table and a decode_char method to decode a single character

---
+++ decode.rb
@@ -0,0 +1,16 @@
+# Define a table of morse code characters and their corresponding letters or numbers.
+MORSE_TABLE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z', '-----' => '0', '.----' => '1', '..---' => '2', '...--' => '3',
+  '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7', '---..' => '8',
+  '----.' => '9'
+}.freeze
+
+# Define a method to decode a single character from morse code.
+def decode_char(char)
+  MORSE_TABLE[char]
+end
